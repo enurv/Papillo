5eeea73ebb7bb97e127866687bc916f5aaa62e5a
enurv <46562699+enurv@users.noreply.github.com>
2020-12-15 17:31:26 +0300

    get quote

--- deneme.js
+++ deneme.js
@@ -17,5 +17,19 @@ function currentTime() {
       return k;
     }
   }
+
+function getQuote() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+      var quote = JSON.parse(this.responseText);
+      document.getElementById("quote").innerHTML = quote.quoteText;
+      }
+    };
+    xhttp.open("GET", "https://papillo.herokuapp.com/", true);
+    xhttp.send();
+}
   
   currentTime(); /* calling currentTime() function to initiate the process */
+  getQuote();
