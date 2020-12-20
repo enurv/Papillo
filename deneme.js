caf2cf284c6212da7c0118e8b130f3ee152c4d1f
enurv <46562699+enurv@users.noreply.github.com>
2020-12-20 21:59:29 +0300

    update on clock yapısı

--- deneme.js
+++ deneme.js
@@ -1,28 +1,41 @@
-function currentTime() {
-    var date = new Date(); /* creating object of Date class */
-    var hour = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
-    hour = updateTime(hour);
-    min = updateTime(min);
-    document.getElementById("clock").innerText = hour + " : " + min; /* adding time to the div */
-      var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
-  }
-  
-  function updateTime(k) {
-    if (k < 10) {
-      return "0" + k;
-    }
-    else {
-      return k;
-    }
+class DigitalClock {
+  constructor(element){
+    this.element = element;
   }
 
+  getTime(){
+    const time = new Date();
+    return {
+      hour: time.getHours(),
+      minute: time.getMinutes()
+    };
+  }
+
+  update() {
+    let time = this.getTime();
+    let hour = time.hour.toString().padStart(2, '0');
+    let minute = time.minute.toString().padStart(2, '0');
+
+    let formattedTime = `${hour}:${minute}`;
+    console.log(this.element.querySelector(".time"));
+    this.element.querySelector(".time").innerHTML = formattedTime;
+  }
+
+  start() {
+    setInterval(()=>{
+      this.update();
+    }, 500);
+  }
+}
+const clockElement = document.querySelector(".clock");
+let digitalclock = new DigitalClock(clockElement);
+digitalclock.start();
+
+//////////////////////////////////
 function getQuote() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-      console.log(this.responseText);
       var quote = JSON.parse(this.responseText);
       document.getElementById("quote").innerHTML = quote.quoteText;
       }
@@ -31,5 +44,4 @@ function getQuote() {
     xhttp.send();
 }
   
-  currentTime(); /* calling currentTime() function to initiate the process */
-  getQuote();
+getQuote();
