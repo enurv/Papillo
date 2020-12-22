960694776d1a2cf8c0d7078e5ae3217e16d3958a
enurv <46562699+enurv@users.noreply.github.com>
2020-12-23 01:35:49 +0300

    pomodoro timer created

---
+++ js/PomodoroTimer.js
@@ -0,0 +1,12 @@
+class PomodoroTimer {
+    constructor(element){
+        this.element = element;
+    }
+
+
+}
+
+function activatePomodoro(){
+    console.log("çalıştı");
+    //document.querySelector(".time").style.visibility = 'hidden';
+}
