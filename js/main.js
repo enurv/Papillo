6c0b1bd4fa18d18e81dd7401774c73f4ad171795
enurv <46562699+enurv@users.noreply.github.com>
2021-01-09 04:07:58 +0300

    switch functionality

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
 
 const clockElement = document.querySelector(".clock");
+const Ptimer = document.querySelector(".pomodoro-timer");
+Ptimer.style.display = "none";
 let digitalclock = new DigitalClock(clockElement);
 digitalclock.start();
 getQuote();
 
 function switchBetween(){
     let clock = document.querySelector(".time");
+    
     if (clock.style.display === "none") {
         clock.style.display = "block";
+        Ptimer.style.display = "none"
     } else {
         clock.style.display = "none";
+        Ptimer.style.display = "block"
     }
 }
 
 document.getElementById("switch").addEventListener("click", function() {
     switchBetween()
 });
+
+
 
 const startButton = document.querySelector('#pomodoro-start');
 const pauseButton = document.querySelector('#pomodoro-pause');
